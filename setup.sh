643cdc7c64c1d86b23670685cd5df0cad6f19251
Takemoto Tetsuro <t@ineeza.com>
2021-02-28 01:16:53 +0900

    Add iTerm2 config

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.tmux.conf ~/
 ln -s ~/dotfiles/.tmux.conf.local ~/
 ln -s ~/dotfiles/.vimrc ~/
 ln -s ~/dotfiles/.spacemacs ~/
+ln -s ~/dotfiles/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
